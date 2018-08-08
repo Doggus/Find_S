6377d25fedeb3c0071998a25c7a94c214dbf11c4
Liron Toledo <tldlir001@myuct.ac.za>
2018-08-08 12:41:47 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+
+    struct training
+    {
+        int sky; // 0 = Sunny, 1 = Rainy
+        int airTemp; // 0 = Warm, 1 = Cold
+        int humidity; // 0 = High, 1 = Normal
+        int wind; // 0 = Strong, 1 = Weak
+        int water; //  0 = Warm, 1 = Cool
+        int forecast; // 0 = Same, 1 = Change
+        int enjoySport; // 0 = Yes, 1 = No
+    };
+
+
+    return 0;
+}
